6b1eb04adc6cbd7a2b45aed16b7cb8c364dffcf3
izzy <izzy@dagsterlabs.com>
2024-10-23 01:39:42 -0600

    moved dbt project to a subfolder and added hybrid github action

--- my_dagster_code_location/project.py
+++ my_dagster_code_location/project.py
@@ -2,9 +2,9 @@ from pathlib import Path
 
 from dagster_dbt import DbtProject
 
-jaffle_shop_project = DbtProject(
-    project_dir=Path(__file__).joinpath("..", "..", "..").resolve(),
+my_dbt_project = DbtProject(
+    project_dir=Path(__file__).joinpath("..", "..", "my_dbt_project").resolve(),
     packaged_project_dir=Path(__file__).joinpath("..", "..", "dbt-project").resolve(),
 )
-jaffle_shop_project.prepare_if_dev()
+my_dbt_project.prepare_if_dev()
 
